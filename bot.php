3cd9945c96324b51cbeb620b6e2bcae63f82633b
Sergey Iryupin <biblelamp@yandex.ru>
2016-01-17 16:58:54 +0300

    Added database

--- bot.php
+++ bot.php
@@ -1,20 +1,23 @@
 <?php
 // Images Processor Bot
-// version 0.2 last update Jan-16-2016
+// version 0.3 last update Jan-17-2016
 // not less PHP 5.2.1 (file_put_contents(), sys_get_temp_dir())
 
 // script's modes
 
+$clear_mode = "clear";
 $shedule_mode = "shedule";
 $download_mode = "download";
 $resize_mode = "resize";
+$list_mode = "list";
 
-// files for queues
+// for queues
 
-$queue_download = "download.txt";
-$queue_failed = "failed.txt";
-$queue_resize = "resize.txt";
-$queue_done = "done.txt";
+$db_name = "bot_queues.sqlite";
+$queue_download = "download";
+$queue_failed = "failed";
+$queue_resize = "resize";
+$queue_done = "done";
 
 // new size for images
 
@@ -22,14 +25,20 @@ $new_width = $new_height = 640;
 
 // messages
 
-$use_messages = PHP_EOL . 'Use: bot (' . $shedule_mode . '|' . $download_mode . '|' . $resize_mode . ')' . PHP_EOL;
+$use_messages = PHP_EOL . 'Use: bot (' .
+	$clear_mode . '|' .
+	$shedule_mode . '|' .
+	$download_mode . '|' .
+	$resize_mode . '|' .
+	$list_mode . ')' . PHP_EOL;
 $use_shedule_message = PHP_EOL . 'Use: bot shedule file_with_links.txt' . PHP_EOL;
 $error_open_file = 'Error: failed to open file ';
+$error_file_not_exists = 'File not exists';
 $urls_processed = ' url(s) processed';
 $dl_queue_empty = PHP_EOL . "Download queue is empty" . PHP_EOL;
 $files_download = ' file(s) downloaded to ';
 $rs_queue_empty = PHP_EOL . "Resize queue is empty" . PHP_EOL;
-$files_resized = ' file(s) resized. See done.txt';
+$files_resized = ' file(s) resized. Use: bot list';
 
 // class for URLs
 
@@ -114,9 +123,23 @@ class imgClass {
 if ($argc == 1) {
     die($use_messages);
 }
-// main procedure
+
+$db = new PDO('sqlite:' . dirname(__FILE__) . DIRECTORY_SEPARATOR . $db_name);
+$db->exec('CREATE TABLE IF NOT EXISTS ' . $queue_download . ' (url TEXT NOT NULL)');
+$db->exec('CREATE TABLE IF NOT EXISTS ' . $queue_failed . ' (url TEXT NOT NULL, status VARCHAR(25) NOT NULL)');
+$db->exec('CREATE TABLE IF NOT EXISTS ' . $queue_resize . ' (path TEXT NOT NULL)');
+$db->exec('CREATE TABLE IF NOT EXISTS ' . $queue_done . ' (path TEXT NOT NULL)');
 
 switch ($argv[1]) {
+
+	// clear mode
+
+	case $clear_mode:
+		$db->exec('DELETE FROM ' . $queue_download);
+		$db->exec('DELETE FROM ' . $queue_failed);
+		$db->exec('DELETE FROM ' . $queue_resize);
+		$db->exec('DELETE FROM ' . $queue_done);
+		break;
 
     // shedule mode
 
@@ -131,9 +154,9 @@ switch ($argv[1]) {
 		foreach($Urls as $url) {
             $img_url = new urlClass($url);
 			if ($img_url->isURLexists()) {
-                file_put_contents($queue_download, $img_url->url . "\n", FILE_APPEND | LOCK_EX);
+				$db->exec('INSERT INTO ' . $queue_download . ' (url) VALUES (\'' . $img_url->url . '\')');
 			} else {
-                file_put_contents($queue_failed, $img_url->url . ' ' . $img_url->getStatus() . "\n", FILE_APPEND | LOCK_EX);
+				$db->exec('INSERT INTO ' . $queue_failed . ' (url, status) VALUES (\'' . $img_url->url . '\', \'' . $img_url->status . '\')');
             }
 		}
 		echo PHP_EOL . count($Urls). $urls_processed . PHP_EOL;
@@ -142,50 +165,83 @@ switch ($argv[1]) {
     // download mode
 
     case $download_mode:
-        if (!file_exists($queue_download)) {
-            die ($dl_queue_empty);
-        }
         $counter = 0;
-        $Urls = file($queue_download, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-        foreach($Urls as $url) {
-            $img_url = new urlClass($url);
+		$Results = $db->query('SELECT * FROM ' . $queue_download)->fetchAll();
+		if (!$Results) {
+			die ($dl_queue_empty);
+		}
+        foreach($Results as $url) {
+            $img_url = new urlClass($url['url']);
 			if ($img_url->isURLexists()) {
                 $image = new imgClass();
                 $image->load($img_url->url);
-                $image->save(sys_get_temp_dir() . '/' . basename($img_url->url));
-                file_put_contents($queue_resize, sys_get_temp_dir() . '/' . basename($img_url->url) . "\n", FILE_APPEND | LOCK_EX);
+                $image->save(sys_get_temp_dir() . DIRECTORY_SEPARATOR . basename($img_url->url));
+				$db->exec('INSERT INTO ' . $queue_resize . ' (path) VALUES (\'' . sys_get_temp_dir() . DIRECTORY_SEPARATOR . basename($img_url->url) . '\')');
                 $counter++;
             } else {
-                file_put_contents($queue_failed, $img_url->url . ' ' . $img_url->getStatus() . "\n", FILE_APPEND | LOCK_EX);
+                $db->exec('INSERT INTO ' . $queue_failed . ' (url, status) VALUES (\'' . $img_url->url . '\', \'' . $img_url->status . '\')');
             }
         }
         echo PHP_EOL . $counter. $files_download . sys_get_temp_dir() . PHP_EOL;
-        unlink($queue_download);
+        $db->exec('DELETE FROM ' . $queue_download);
         break;
 
     // resize mode
 
     case $resize_mode:
-        if (!file_exists($queue_resize)) {
-            die ($rs_queue_empty);
-        }
-        $counter = 0;
-        $Images = file($queue_resize, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-        foreach($Images as $img_path) {
-            if (file_exists($img_path)) {
+		$counter = 0;
+		$Results = $db->query('SELECT * FROM ' . $queue_resize)->fetchAll();
+		if (!$Results) {
+			die ($rs_queue_empty);
+		}
+        foreach($Results as $img) {
+            if (file_exists($img['path'])) {
                 $image = new imgClass();
-                $image->load($img_path);
+                $image->load($img['path']);
                 $image->resize($new_width, $new_height);
-                $image->save(basename($img_path));
-                file_put_contents($queue_done, basename($img_path) . "\n", FILE_APPEND | LOCK_EX);
+                $image->save(basename($img['path']));
+				$db->exec('INSERT INTO ' . $queue_done . ' (path) VALUES (\'' . basename($img['path']) . '\')');
                 $counter++;
             } else {
-                file_put_contents($queue_failed, $img_path . "\n", FILE_APPEND | LOCK_EX);
+				$db->exec('INSERT INTO ' . $queue_failed . ' (url, status) VALUES (\'' . $img['path'] . '\', \'' . $error_file_not_exists . '\')');
             }
         }
         echo PHP_EOL . $counter . $files_resized . PHP_EOL;
-        unlink($queue_resize);
+        $db->exec('DELETE FROM ' . $queue_resize);
         break;
+
+	// list mode
+
+	case $list_mode:
+		$Results = $db->query('SELECT * FROM ' . $queue_download)->fetchAll();
+		if ($Results) {
+			echo PHP_EOL . '::' . $queue_download . PHP_EOL;
+		}
+		foreach ($Results as $row) {
+			echo $row['url'] . PHP_EOL;
+		}
+		$Results = $db->query('SELECT * FROM ' . $queue_failed)->fetchAll();
+		if ($Results) {
+			echo PHP_EOL . '::' . $queue_failed . PHP_EOL;
+		}
+		foreach ($Results as $row) {
+			echo $row['url'] . ' ' . $row['status'] . PHP_EOL;
+		}
+		$Results = $db->query('SELECT * FROM ' . $queue_resize)->fetchAll();
+		if ($Results) {
+			echo PHP_EOL . '::' . $queue_resize . PHP_EOL;
+		}
+		foreach ($Results as $row) {
+			echo $row['path'] . PHP_EOL;
+		}
+		$Results = $db->query('SELECT * FROM ' . $queue_done)->fetchAll();
+		if ($Results) {
+			echo PHP_EOL . '::' . $queue_done . PHP_EOL;
+		}
+		foreach ($Results as $row) {
+			echo $row['path'] . PHP_EOL;
+		}
+		break;
 
     default:
         die($use_messages);
